49bddee9d7ed8c4dff55adc7787c000859d9c684
Dmitry <dima.soin2014@yandex.ru>
2025-12-09 21:28:34 +0300

    class Course and Lesson

--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(max_length=300, verbose_name='Название курса')
+    preview = models.ImageField(blank=True, null=True, verbose_name='Превью (картинка)')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=300, verbose_name='Название урока')
+    preview = models.ImageField(blank=True, null=True, verbose_name='Превью (картинка)')
+    video_url = models.URLField(blank=True, null=True, verbose_name='Ссылка на видео')
+    сourse = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Курс')
