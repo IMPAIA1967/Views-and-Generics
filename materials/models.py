9bab5f9b797b58a58e0cfc8e2610f738d8229306
Dmitry <dima.soin2014@yandex.ru>
2025-12-23 22:14:06 +0300

    tests

--- materials/models.py
+++ materials/models.py
@@ -5,12 +5,13 @@ class Course(models.Model):
     title = models.CharField(max_length=300, verbose_name='Название курса')
     preview = models.ImageField(blank=True, null=True, verbose_name='Превью (картинка)')
     description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    owner = models.ForeignKey('users.User', on_delete=models.CASCADE, null=True, blank=True)
 
 class Lesson(models.Model):
     title = models.CharField(max_length=300, verbose_name='Название урока')
     preview = models.ImageField(blank=True, null=True, verbose_name='Превью (картинка)')
     video_url = models.URLField(blank=True, null=True, verbose_name='Ссылка на видео')
-    сourse = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Курс')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Курс')
     owner = models.ForeignKey('users.User', on_delete=models.CASCADE, null=True, blank=True, verbose_name='Владелец')
 
 class Subscription(models.Model):
--- materials/tests.py
+++ materials/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.contrib.auth import get_user_model
+from .models import Course, Lesson
 
-# Create your tests here.
+
+User = get_user_model()
+
+class MyTests(APITestCase):
+    def test_user_can_create_lesson(self):
+
+        #  Создаём пользователя
+        user = User.objects.create_user(username='test@test.com', password='123')
+
+        #  Создаём курс
+        course = Course.objects.create(title='Мой курс', owner=user)
+
+        # Делаем вид, что пользователь зашёл на сайт
+        self.client.force_authenticate(user=user)
+
+        # Пытаемся создать урок
+        response = self.client.post('/api/lessons/create/', {
+            'title': 'Новый урок',
+            'video_url': 'https://www.youtube.com/watch?v=123',
+            'course': course.id
+        })
+        print("Response data:", response.data)
+        self.assertEqual(response.status_code, 201)  # успешно создано
+        self.assertEqual(Lesson.objects.count(), 1)  # должен быть 1 урок
